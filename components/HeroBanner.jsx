52d8255e7af223a75f60cb10090c2a4686d6663a
icecrimu <charles.banigon@gmail.com>
2022-05-19 22:13:00 +0800

    Hook up Sanity

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -1,21 +1,28 @@
 import Link from 'next/link'
 import React from 'react'
 
-export default function HeroBanner() {
+import { urlFor } from '../lib/client'
+
+export default function HeroBanner({ heroBanner }) {
   return (
     <div className="hero-banner-container">
       <div>
-        <p className="beats-solo">SMALL TEXT</p>
-        <h3>MID TEXT</h3>
-        <img src="" alt="headphones" className="hero-banner-image" />
+        <p className="beats-solo">{heroBanner.smallText}</p>
+        <h3>{heroBanner.midText}</h3>
+        <h1>{heroBanner.largeText1}</h1>
+        <img
+          src={urlFor(heroBanner.image)}
+          alt={heroBanner.product}
+          className="hero-banner-image"
+        />
 
         <div>
-          <Link href="/product/ID">
-            <button type="button">BUTTON TEXT</button>
+          <Link href={`/product/${heroBanner.product}`}>
+            <button type="button">{heroBanner.buttonText}</button>
           </Link>
           <div className="desc">
             <h5>Description</h5>
-            <p>DESCRIPTIONS</p>
+            <p>{heroBanner.desc}</p>
           </div>
         </div>
       </div>
