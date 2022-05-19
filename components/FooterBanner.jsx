450166715d07ea2293fa9f2de2349db559d9d373
icecrimu <charles.banigon@gmail.com>
2022-05-19 22:46:54 +0800

    Products

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,5 +1,45 @@
 import React from 'react'
+import Link from 'next/link'
 
-export default function FooterBanner() {
-  return <div>FooterBanner</div>
+import { urlFor } from '../lib/client'
+
+export default function FooterBanner({
+  footerBanner: {
+    discount,
+    largeText1,
+    largeText2,
+    saleTime,
+    smallText,
+    midText,
+    desc,
+    product,
+    buttonText,
+    image
+  }
+}) {
+  return (
+    <div className="footer-banner-container">
+      <div className="banner-desc">
+        <div className="left">
+          <p>{discount}</p>
+          <h3>{largeText1}</h3>
+          <h3>{largeText2}</h3>
+          <p>{saleTime}</p>
+        </div>
+        <div className="right">
+          <p>{smallText}</p>
+          <h3>{midText}</h3>
+          <p>{desc}</p>
+          <Link href={`/product/${product}`}>
+            <button type="button">{buttonText}</button>
+          </Link>
+        </div>
+        <img
+          src={urlFor(image)}
+          alt={product}
+          className="footer-banner-image"
+        />
+      </div>
+    </div>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,11 @@ export default function Home({ products, bannerData }) {
         <p>Speakers of many variations</p>
       </div>
       <div className="products-container">
-        {products?.map(product => product.name)}
+        {products?.map(product => (
+          <Product key={product._id} product={product} />
+        ))}
       </div>
-      <FooterBanner />
+      <FooterBanner footerBanner={bannerData && bannerData[0]} />
     </>
   )
 }
